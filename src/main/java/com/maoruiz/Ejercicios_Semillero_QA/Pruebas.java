a41e74da5d774a5c3bd4bedf9fa5535999bbb4b5
maoruizh <maoruizh@gmail.com>
2022-10-25 15:27:02 -0500

    se dejan listos los ejercicios del 1 al 9 menos el 7

--- src/main/java/com/maoruiz/Ejercicios_Semillero_QA/Pruebas.java
+++ src/main/java/com/maoruiz/Ejercicios_Semillero_QA/Pruebas.java
@@ -5,26 +5,22 @@ import java.util.Scanner;
 
 public class Pruebas {
     public static void main(String[] args) {
-       /* System.out.println("\nEste es el resultado del ejercicio # 9");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un elemento a buscar: ");
-        int busqueda;
-        busqueda = sc.nextInt();
-        int vector[] = new int[]{3, 25, 39, 54};
-        boolean encontro = false;
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i] == (busqueda)) {
-                System.out.println("Se encontro el elemento en la posición: " + i);
-                encontro = true;
-            }
-        }
-        if (encontro == false) {
-            System.out.println("No se encontro");
-        }
-        System.out.println("-----------------------------------------------------------------------------------------");
-*/
 
-       // Pedir dos palabras por teclado, indicar si son iguales.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el primer nombre:");
+        String nombre1;
+        nombre1 = sc.nextLine();
+        System.out.print("Introduce el segundo nombre:");
+        String nombre2;
+        nombre2 = sc.nextLine();
+
+        System.out.println("El primer nombre es " + nombre1 + " y el segundo nombre es " + nombre2);
+        if (nombre1.equals(nombre2)) {
+            System.out.println("Los dos nombres son iguales"); //No está funcionando bien
+        } else System.out.println("Los nombres son diferentes");
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+    /* Pedir dos palabras por teclado, indicar si son iguales.
         System.out.println("\nEste es el resultado del ejercicio # 4");
         String nombre1 = JOptionPane.showInputDialog("Introduce el primer nombre");
         String nombre2 = JOptionPane.showInputDialog("Introduce el segundo nombre");
@@ -33,8 +29,11 @@ public class Pruebas {
             System.out.println("Los dos nombres son iguales"); //No está funcionando bien
         } else System.out.println("Los nombres son diferentes");
         System.out.println("-----------------------------------------------------------------------------------------");
+*/
     }
 }
+
+
 /*
 
 Scanner
